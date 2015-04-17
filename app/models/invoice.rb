114979dcc57131e597fbf77b78baaf789536ad31
Charles Kim <ckim0706@gmail.com>
2015-04-17 01:04:55 -0700

    implemented account information for pdf

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,12 +1,34 @@
 require 'csv'
 
 class Invoice < ActiveRecord::Base
+  after_create :add_account_info
 
   def self.import(file)
     CSV.foreach(file.path, headers:true) do |row|
       invoice_hash = row.to_hash
-      Invoice.create!(invoice_hash)
+      Invoice.create(invoice_hash)
     end
   end
 
+  private
+
+  def add_account_info
+    info = {"김성일"=>"443137-91-100082",
+            "박삼인"=>"443102-01-227335",
+            "김형선"=>"443102-01-227322"}
+
+    self.account_location = "국민은행"
+    
+    case self.floor_id[0]
+    when "1"
+      self.account_number = info["김형선"]
+      self.account_name = info.keys[2]
+    when "2"
+      self.account_number == info["박삼인"]
+      self.account_name == info.keys[1]
+    else
+      self.account_number == info["김성일"]
+      self.account_name == info.keys[0]
+    end
+  end
 end
--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -1,15 +1,53 @@
 class OrderPdf < Prawn::Document
+  
   def initialize(invoice)
     super(top_margin: 70)
     @invoice = invoice
+    font_update
+    
+    header
     invoice_number
+
+
+    account
+    footer    
+  end
+
+  def font_update
+    font_families.update(
+      "Unbatang" => { :normal => Rails.root.join('app/assets/fonts/unbatang.ttf').to_s,
+                      # :normal => Rails.root.join('app/assets/fonts/unbatangbold.ttf').to_s 
+                    }
+    )
+    font "Unbatang"
   end
 
   def invoice_number
-    text "Invoice \##{@invoice.floor_area} #{@invoice.name}"
+    text "청구서 \##{@invoice.floor_id} #{@invoice.name}"
+  end
+
+  def header
+    text "청구서"
+  end
+
+  def recipient 
+    text "#{@invoice.name}"
+  end
+
+  def payment_due_date
+    text ""
   end
 
 
+
+  def account
+    text "위 금액을 #{@invoice.account_location} #{@invoice.account_number} (#{@invoice.account_name}) 계좌로 입금시켜 주시기 바랍니다."
+  end
+
+
+  def footer
+    text "만수빌딩 대표   김 성일"
+  end
 
 
 end
