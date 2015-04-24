a81d3761576bafce8a2bea99fb25e1d84236323a
Charles Kim <ckim0706@gmail.com>
2015-04-23 20:13:38 -0700

    changed the db from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150416080115) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "invoices", force: :cascade do |t|
     t.string   "floor_id"
     t.string   "name"
